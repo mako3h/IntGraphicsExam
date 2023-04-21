07fab678261e0d64a38bdc86efa791c5d9fdc7b1
mako3h <reidplayter3@gmail.com>
2023-04-21 14:23:05 -0400

    finalLastPush

--- Assets/Exam/Scripts/MovingPerson.cs
+++ Assets/Exam/Scripts/MovingPerson.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class MovingPerson : MonoBehaviour
 {
-    public float rotSpeed = 10.0f;
+    public float rotSpeed = 146.9f;
     public Vector3 midPoint = Vector3.zero;
 
     void Update()
     {
-        //this makes the sphere rotate around, i added the mid point in there, and you can set the xyz, in the inspector, that the sphere will rotate around
+      
         transform.RotateAround(midPoint, Vector3.up, rotSpeed * Time.deltaTime);
 
     }
